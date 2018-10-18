b8b53b1b9a07b0402df023db87c3b79cb82cfec5
Taras Kovtun <taraskovtun@Tarass-MacBook-Pro.local>
2018-10-17 21:47:11 -0700

    initial

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { Provider , connect} from 'react-redux'
+import { createStore, combineReducers, bindActionCreators }  from 'redux';
+
+import 'sanitize.css/sanitize.css'
+import './index.css'
+
+const target = document.querySelector('#root')
+
+const INCREMENT_COUNTER = 'increment_counter'
+const DECREMENT_COUNTER = 'decrement_counter'
+
+let countersReducer = function(state = 0, action) {
+    switch(action.type) {
+        
+      case DECREMENT_COUNTER:
+        state = state- 1;
+        return state;
+      case INCREMENT_COUNTER:
+        state = state + 1;
+        return state;
+      default:
+        return state;
+    }
+  }
+
+const initialState = {counter: 0};
+
+const rootReducer = combineReducers ({
+    counter: countersReducer
+  });
+
+let store = createStore(rootReducer, initialState);
+
+const increment = ()=>({type: INCREMENT_COUNTER})
+const decrement = ()=>({type: DECREMENT_COUNTER})
+
+const Board = (props)=><div>
+    <div>Counter: {props.counter}</div>
+    <button onClick={props.increment}>Increment</button>
+    <button onClick={props.decrement}>Decrement</button>
+    </div>
+
+const App = connect(
+    state => ({counter: state.counter, state}),
+
+    dispatch =>
+        bindActionCreators(
+            {
+                increment,
+                decrement
+            },
+            dispatch
+        )
+)(Board)
+
+render(
+  <Provider store={store}>
+        <App className='app' />
+  </Provider>,
+  target
+)
